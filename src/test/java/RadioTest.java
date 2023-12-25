50d8ae574bda5d5862bcbecc6489936fc2f81236
YuriS <yuri-s@list.ru>
2023-12-26 00:49:57 +0300

    Deffect code commit

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class RadioTest {
+public class RadioTest {9
 
     @Test
     public void testNextStations() {
